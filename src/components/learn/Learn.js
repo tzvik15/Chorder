ad88444b3e29f1b11fb297c476f981c38b1f8199
tzvik15 <tzvik15@yahoo.com>
2020-04-17 10:58:05 -0600

    started writing triad theory, successful use of sound files

--- src/components/learn/Learn.js
+++ src/components/learn/Learn.js
@@ -1,6 +1,14 @@
 import React, {useState} from "react";
 import "./learn.css";
-import {Link} from "react-router-dom"
+import {Link} from "react-router-dom";
+import useSound from 'use-sound';
+
+import audio1 from "../../assets/sounds/M.wav";
+import audio2 from "../../assets/sounds/minor.wav";
+import audio3 from "../../assets/sounds/dim_tri.wav";
+import audio4 from "../../assets/sounds/aug_tri.wav";
+
+
 
 
 
@@ -8,7 +16,13 @@ import {Link} from "react-router-dom"
 
 
 const Learn =()=> {
-    
+
+    const [playMaj] = useSound(audio1);
+    const [playmin] = useSound(audio2);
+    const [playDim] = useSound(audio3);
+    const [playAug] = useSound(audio4);
+
+
     const [buttons, setButtons] = useState({
         q1: false,
         triads: false,
@@ -32,6 +46,7 @@ const Learn =()=> {
         console.log(buttons)
     }
 
+
     return (
         buttons.q1===false?
         <>
@@ -46,7 +61,17 @@ const Learn =()=> {
         :
         buttons.q1===true && buttons.septa===false && buttons.triads===true && buttons.inversions===false?
         <>
-        <h2>triad stuff</h2>
+        <h1>Triads</h1>
+        <p>At the most basic level, a triad is a collection of 3 notes, where the interval space between each pair of notes is a (major or minor) third. To simplify this concept, consider the order of notes: C, D, E, F, G, A, B, C. for a triad to exist, the three notes most be 2 names apart from each other (C-->E, D-->F, etc.). Those three notes can come in any order. The way they are ordered are called "inversions". (To learn about inversions, click the "inversions" button). When presented with a group of 3 notes, first see if you can stack them to form a simple (non inverted) triad. For now disregard any "#" and "b" signs: only stack them by note names. Also note that any number of the 3 notes may be repeated: in music theory, there is no difference between [C,E,G] to [C,E,G,C,G]: they are both considered a triad and the same to one another.</p>
+        <h2>Triad types, and how to differentiate between them</h2>
+        <p>There are four (4) types of triads in Western Classical Music. Each one of these types sounds differently when played. The type of triad depends on the type and order of thirds. As mentioned, triads are a stack of 2 thirds. Both of those thirds can be either major ("M"), or minor ("m"). That means that the interval content of a triad can be one of these 4 options: MM, Mm, mM, mm. Each of those permutations has a name in music theory: Mm: a Major chord, mM: a Minor chord, MM: an Augmented chord, mm: a Diminished chord. All of these names also have an accepted shorthand: Major: M, Minor: m, Augmented: Aug. , Diminished: Dim. </p>
+        <p>An interesting by-product of how triads are created, by stacking 2 thirds, there is also a fifth interval created - from the lowest to the highest note. In M and m chords, the fifth is perfect. In Dim chords the fifth is diminished (also known as a "tritone"), and in Aug chords the fifth is augmented. This fifth interval helps create the unique sound of each of the triads. For an example of how each of the 4 triads sounds, click the buttons below.</p>
+        <div className="sounds">
+            <button onClick={playMaj}>Major (M)</button>
+            <button onClick={playmin}>Minor (m)</button>
+            <button onClick={playAug}>Augmented (Aug)</button>
+            <button onClick={playDim}>Diminished (Dim)</button>
+        </div>
         <button onClick={inv}>Inversions</button>
         <button onClick={sept}>Septachords</button>
         <button> <Link to="/">HOME!</Link></button>
